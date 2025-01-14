b8c27b991065fdfe98487dbd4f1ccf2db2ddf2de
petergsme <garciasan@esat-alumni.com>
2025-01-14 19:04:22 +0100

    deberes corregidos

--- ejerciciosClase1PN.js
+++ ejerciciosClase1PN.js
@@ -1,11 +1,11 @@
 //Repaso previo de los métodos de arrays y como identificar sobre lo que se está operando.
 const greetings = (names) => {
-  return names.split(' ').forEach((name) => {
+  return names.split(" ").forEach((name) => {
     console.log(`¡Hola ${name}!`);
   });
 };
 
-greetings('pepe juana banesa');
+greetings("pepe juana banesa");
 
 //Deberes de fechas.
 /**
@@ -44,13 +44,13 @@ const sumTime = (date, time, unitToSum) => {
   const unitNormalize = unitToSum.toLowerCase();
   let timeInMs = time * 24 * 60 * 60 * 1000;
 
-  if (unitNormalize === 'h') {
+  if (unitNormalize === "h") {
     timeInMs = time * 60 * 60 * 1000;
   }
-  if (unitNormalize === 'min') {
+  if (unitNormalize === "min") {
     timeInMs = time * 60 * 1000;
   }
-  if (unitNormalize === 's') {
+  if (unitNormalize === "s") {
     timeInMs = time * 1000;
   }
 
@@ -59,10 +59,10 @@ const sumTime = (date, time, unitToSum) => {
 
 const dateToTry3 = new Date();
 
-console.log(sumTime(dateToTry3, 8, 'd'));
-console.log(sumTime(dateToTry3, 8, 'h'));
-console.log(sumTime(dateToTry3, 8, 'min'));
-console.log(sumTime(dateToTry3, 8, 's'));
+console.log(sumTime(dateToTry3, 8, "d"));
+console.log(sumTime(dateToTry3, 8, "h"));
+console.log(sumTime(dateToTry3, 8, "min"));
+console.log(sumTime(dateToTry3, 8, "s"));
 
 /**
  * 4.  Modifica la función del ejercicio 2 para que sea más genérica y que permita restar días, horas, minutos o segundos
@@ -72,13 +72,13 @@ const minusTime = (date, time, unitToSum) => {
   const unitNormalize = unitToSum.toLowerCase();
   let timeInMs = time * 24 * 60 * 60 * 1000;
 
-  if (unitNormalize === 'h') {
+  if (unitNormalize === "h") {
     timeInMs = time * 60 * 60 * 1000;
   }
-  if (unitNormalize === 'min') {
+  if (unitNormalize === "min") {
     timeInMs = time * 60 * 1000;
   }
-  if (unitNormalize === 's') {
+  if (unitNormalize === "s") {
     timeInMs = time * 1000;
   }
 
@@ -87,10 +87,10 @@ const minusTime = (date, time, unitToSum) => {
 
 const dateToTry4 = new Date();
 
-console.log(minusTime(dateToTry4, 12, 'd'));
-console.log(minusTime(dateToTry4, 8, 'h'));
-console.log(minusTime(dateToTry4, 45, 'min'));
-console.log(minusTime(dateToTry4, 98, 's'));
+console.log(minusTime(dateToTry4, 12, "d"));
+console.log(minusTime(dateToTry4, 8, "h"));
+console.log(minusTime(dateToTry4, 45, "min"));
+console.log(minusTime(dateToTry4, 98, "s"));
 
 /**
  * 5. Crea una función que compruebe si una fecha está entre otras dos fechas.
@@ -100,9 +100,9 @@ const isBetweenDates = (firstDate, middleDate, lastDate) => {
   return middleDate > firstDate && middleDate < lastDate;
 };
 
-const dateToTry5 = new Date('2025-01-11T14:02:00.000Z'); //año 2025, mes 1(enero), día 11, 14:02.
+const dateToTry5 = new Date("2025-01-11T14:02:00.000Z"); //año 2025, mes 1(enero), día 11, 14:02.
 const dateToTry6 = new Date(2025, 0, 11, 16); //año 2025, mes 0 (enero), día 11, 16:00.
-const dateToTry7 = new Date('01/12/25'); //enero, día 12, año 2025.
+const dateToTry7 = new Date("01/12/25"); //enero, día 12, año 2025.
 
 console.log(isBetweenDates(dateToTry5, dateToTry6, dateToTry7));
 console.log(isBetweenDates(dateToTry6, dateToTry5, dateToTry7));
@@ -120,41 +120,63 @@ const DAYS_IN_MILLISECONDS = HOURS_IN_MILLISECONDS * 24;
 
 const timePassed = (fromDate, disableOutput = {}) => {
   //Es importante predeclarar disableOutput como objeto para que tenga un valor y pueda usarse la desestructuración del objeto. En caso contrario, cuando no pasáramos ningún parametro en su lugar sería undefined y no nos permitiría aplicar sobre él la desestructuración.
-  const { days = true, hours = true, minutes = true, seconds = true } = disableOutput;
 
-  const dateNow = new Date();
-  const timeBetweenThenAndNow = Math.abs(fromDate.getTime() - dateNow.getTime());
-
-  if (fromDate > dateNow) {
-    return 'Esa fecha no ha llegado aún.';
+  if (fromDate > new Date()) {
+    return "Esa fecha no ha llegado aún.";
   }
 
-  const day = Math.floor(timeBetweenThenAndNow / DAYS_IN_MILLISECONDS);
-  const hour = Math.floor((timeBetweenThenAndNow % DAYS_IN_MILLISECONDS) / HOURS_IN_MILLISECONDS);
-  const min = Math.floor((timeBetweenThenAndNow % HOURS_IN_MILLISECONDS) / MINUTES_IN_MILLISECONDS);
-  const sec = Math.floor((timeBetweenThenAndNow / SECONDS_IN_MILLISECONDS) % 60);
+  const { days = true, hours = true, minutes = true, seconds = true } = disableOutput;
 
-  const dayText = day !== 0 ? ` ${day} días,` : '';
-  const hourText = hour !== 0 ? ` ${hour} horas,` : '';
-  const minText = min !== 0 ? ` ${min} minutos` : '';
-  const secText = sec !== 0 ? ` ${sec} segundos` : '';
+  const timeBetweenThenAndNow = Date.now() - fromDate.getTime();
 
-  const connectSentence = minText && secText ? ' y' : '';
+  const day = days ? Math.floor(timeBetweenThenAndNow / DAYS_IN_MILLISECONDS) : 0;
+  let leftTime = days ? timeBetweenThenAndNow % DAYS_IN_MILLISECONDS : timeBetweenThenAndNow;
 
-  const sameTime = timeBetweenThenAndNow === 0 ? ' nada, estás comparando las mismas horas' : '';
+  const hour = hours ? Math.floor(leftTime / HOURS_IN_MILLISECONDS) : 0;
+  leftTime = hours ? leftTime % HOURS_IN_MILLISECONDS : leftTime;
 
-  return `Desde ${fromDate.toLocaleString()} hasta ahora, han pasado:${days ? dayText : ''}${hours ? hourText : ''}${
-    minutes ? minText : ''
-  }${seconds ? connectSentence : ''}${seconds ? secText : ''}${sameTime}.`;
+  const min = minutes ? Math.floor(leftTime / MINUTES_IN_MILLISECONDS) : 0;
+  leftTime = minutes ? leftTime % HOURS_IN_MILLISECONDS : leftTime;
+
+  const sec = seconds ? Math.floor(leftTime / SECONDS_IN_MILLISECONDS) : 0;
+
+  const dayText = days && timeBetweenThenAndNow >= DAYS_IN_MILLISECONDS ? ` ${day} días,` : "";
+  const hourText = hours && timeBetweenThenAndNow >= HOURS_IN_MILLISECONDS ? ` ${hour} horas,` : "";
+  const minText = minutes && timeBetweenThenAndNow >= MINUTES_IN_MILLISECONDS ? ` ${min} minutos` : "";
+  const secText = seconds && timeBetweenThenAndNow >= 1000 ? ` ${sec} segundos` : "";
+
+  const connectSentence = minText && secText ? " y" : "";
+
+  const sameTime = timeBetweenThenAndNow === 0 ? " nada, estás comparando las mismas horas" : "";
+
+  return `Desde ${fromDate.toLocaleString()} hasta ahora, han pasado:${days ? dayText : ""}${hours ? hourText : ""}${
+    minutes ? minText : ""
+  }${seconds ? connectSentence : ""}${seconds ? secText : ""}${sameTime}.`;
 };
 
-const dateToTry8 = new Date('2022-05-23T01:02:45.000Z');
-const dateToTry9 = new Date('2025-01-11T15:02:45.000Z');
-
-console.log(timePassed(new Date()));
-console.log(timePassed(dateToTry8));
-console.log(timePassed(dateToTry9));
-console.log(timePassed(dateToTry8, { days: false, hours: false }));
+console.log("normal", timePassed(new Date(Date.now() - DAYS_IN_MILLISECONDS)));
+console.log("sin dias", timePassed(new Date(Date.now() - DAYS_IN_MILLISECONDS), { days: false }));
+console.log(
+  "sin dias ni minutos",
+  timePassed(new Date(Date.now() - DAYS_IN_MILLISECONDS), { days: false, hours: false })
+);
+console.log(
+  "solo segundos",
+  timePassed(new Date(Date.now() - DAYS_IN_MILLISECONDS), { days: false, hours: false, minutes: false })
+);
+console.log(
+  "sin nada",
+  timePassed(new Date(Date.now() - DAYS_IN_MILLISECONDS), { days: false, hours: false, minutes: false, seconds: false })
+);
+console.log(
+  "con dias y minutos",
+  timePassed(new Date(Date.now() - DAYS_IN_MILLISECONDS - 80000), {
+    days: true,
+    hours: false,
+    minutes: true,
+    seconds: false,
+  })
+);
 
 /**
  * 7. Si no lo has hecho, modifica la función anterior para que no salga la información que no sea relevante. Por ejemplo, si solo han pasado
@@ -174,6 +196,8 @@ console.log(timePassed(dateToTry8, { days: false, hours: false }));
  * 9. Crea una función como la anterior, pero que indique cuánto tiempo queda para una fecha específica.
  */
 
+//Debería ser como el de arriba, demasiado chungo. Pero ahí está.
+
 const timeUntil = (toDate, disableOutput = {}) => {
   const { days = true, hours = true, minutes = true, seconds = true } = disableOutput;
 
@@ -181,7 +205,7 @@ const timeUntil = (toDate, disableOutput = {}) => {
   const timeBetweenThenAndNow = Math.abs(toDate.getTime() - dateNow.getTime());
 
   if (toDate < dateNow) {
-    return 'Esa fecha ya llegó, y se fue.';
+    return "Esa fecha ya llegó, y se fue.";
   }
 
   const day = Math.floor(timeBetweenThenAndNow / DAYS_IN_MILLISECONDS);
@@ -189,22 +213,22 @@ const timeUntil = (toDate, disableOutput = {}) => {
   const min = Math.floor((timeBetweenThenAndNow % HOURS_IN_MILLISECONDS) / MINUTES_IN_MILLISECONDS);
   const sec = Math.floor((timeBetweenThenAndNow / SECONDS_IN_MILLISECONDS) % 60);
 
-  const dayText = day !== 0 ? ` ${day} días,` : '';
-  const hourText = hour !== 0 ? ` ${hour} horas,` : '';
-  const minText = min !== 0 ? ` ${min} minutos` : '';
-  const secText = sec !== 0 ? ` ${sec} segundos` : '';
+  const dayText = day !== 0 ? ` ${day} días,` : "";
+  const hourText = hour !== 0 ? ` ${hour} horas,` : "";
+  const minText = min !== 0 ? ` ${min} minutos` : "";
+  const secText = sec !== 0 ? ` ${sec} segundos` : "";
 
-  const connectSentence = minText && secText ? ' y' : '';
+  const connectSentence = minText && secText ? " y" : "";
 
-  const sameTime = timeBetweenThenAndNow === 0 ? ' nada, estás comparando las mismas horas' : '';
+  const sameTime = timeBetweenThenAndNow === 0 ? " nada, estás comparando las mismas horas" : "";
 
-  return `Desde ahora hasta ${toDate.toLocaleString()} pasarán:${days ? dayText : ''}${hours ? hourText : ''}${
-    minutes ? minText : ''
-  }${seconds ? connectSentence : ''}${seconds ? secText : ''}${sameTime}.`;
+  return `Desde ahora hasta ${toDate.toLocaleString()} pasarán:${days ? dayText : ""}${hours ? hourText : ""}${
+    minutes ? minText : ""
+  }${seconds ? connectSentence : ""}${seconds ? secText : ""}${sameTime}.`;
 };
 
-const dateToTry10 = new Date('2026-05-23T01:02:45.000Z');
-const dateToTry11 = new Date('2022-01-11T15:02:45.000Z');
+const dateToTry10 = new Date("2026-05-23T01:02:45.000Z");
+const dateToTry11 = new Date("2022-01-11T15:02:45.000Z");
 
 console.log(timeUntil(new Date()));
 console.log(timeUntil(dateToTry10));
@@ -217,29 +241,29 @@ console.log(timeUntil(dateToTry10, { days: false, hours: false }));
 
 const tasks = [
   {
-    text: 'Hacer la compra',
+    text: "Hacer la compra",
     completed: true,
-    completedAt: '2025-01-10T15:54:40.088Z',
+    completedAt: "2025-01-10T15:54:40.088Z",
   },
   {
-    text: 'Ir a clase',
+    text: "Ir a clase",
     completed: true,
-    completedAt: '2025-01-09T15:00:40.088Z',
+    completedAt: "2025-01-09T15:00:40.088Z",
   },
   {
-    text: 'Comer a clase',
+    text: "Comer a clase",
     completed: true,
-    completedAt: '2025-01-09T14:00:40.088Z',
+    completedAt: "2025-01-09T14:00:40.088Z",
   },
   {
-    text: 'Repasar javascript',
+    text: "Repasar javascript",
     completed: false,
   },
 ];
 
 const tasksDoneJanuary9 = (tasks) => {
-  const startOfDay = new Date('2025-01-09T00:00:00.000Z');
-  const endOfDay = new Date('2025-01-10T00:00:00.000Z');
+  const startOfDay = new Date("2025-01-09T00:00:00.000Z");
+  const endOfDay = new Date("2025-01-10T00:00:00.000Z");
 
   return tasks.filter((task) => {
     return isBetweenDates(startOfDay, new Date(task.completedAt), endOfDay);
