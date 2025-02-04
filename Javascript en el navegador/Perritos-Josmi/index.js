5078b5a6a157884c9a17d2f5f0e171858b6ea190
petergsme <garciasan@esat-alumni.com>
2025-02-04 20:00:00 +0100

    sasasa

--- Javascript en el navegador/Perritos-Josmi/index.js
+++ Javascript en el navegador/Perritos-Josmi/index.js
@@ -1,6 +1,6 @@
 const perricosArray = [
-  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
-  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
+  "https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg",
+  "https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg",
 ];
 //'perricosArray' es el contenedor de las imágenes de los dos primeros perricos que se pintan en pantalla por defecto al cargar la página.
 
@@ -11,14 +11,14 @@ SECCIÓN INICIAL DE TIMEOUT
 */
 
 const timeOutId = setTimeout(() => {
-  document.querySelector('#alert').style.display = 'inline-block';
+  document.querySelector("#alert").style.display = "inline-block";
 }, 3000); // A los 10 segundos muestra un texto que te dije que clickes un fucking boton.
 
 // De dejar en mi caso display vacío para que intentara mostrarse no iría. Esto solo funciona cuando ese display none lo has metido en su etiqueta de html como propiedad. Si lo metiste con CSS tienes que darle un bloque real.
 
 function clearWarningText() {
   clearTimeout(timeOutId);
-  document.querySelector('#alert').style.display = 'none';
+  document.querySelector("#alert").style.display = "none";
 } //Cuando clicas un boton llamas a esta funcion, sirve para que no se muestre el texto que dice que clickes y se carga el timeout formalmente.
 
 /*
@@ -28,23 +28,22 @@ FUNCIONES BÁSICAS PARA AÑADIR PERRICOS Y DAR LISTENERS A LOS BOTONES DE SUS TA
 */
 
 const giveDogNodeListeners = (dogNode) => {
-  const likeButton = dogNode.querySelector('.like');
-  const dislikeButton = dogNode.querySelector('.dislike');
+  const likeButton = dogNode.querySelector(".like");
+  const dislikeButton = dogNode.querySelector(".dislike");
 
-  dogNode.querySelector('.like').addEventListener('click', () => {
-    const parrafo = likeButton.previousElementSibling; //Desde el botón seleccionamos su hermano anterior, el párrafo con contadores.
-    const likeCountNode = parrafo.querySelector('.like-count'); //Guardamos la posición del contador positivo en una variable.
+  likeButton.addEventListener("click", () => {
+    const likeCountNode = dogNode.querySelector(".like-count"); //Guardamos la posición del contador positivo en una variable.
     likeCountNode.innerText = Number(likeCountNode.innerText) + 1; //Convertimos a número su interior y sumamos 1.
   });
 
-  dogNode.querySelector('.dislike').addEventListener('click', () => {
-    const dislikeCountNode = dislikeButton.closest('.card').querySelector('.dislike-count'); // Ojito a como usamos aquí closest, que solo busca hacia arriba, llegamos arriba y bajamos.
+  dislikeButton.addEventListener("click", () => {
+    const dislikeCountNode = dogNode.querySelector(".dislike-count");
     dislikeCountNode.innerText = Number(dislikeCountNode.innerText) + 1;
   });
 };
 
 function renderPerricoArray() {
-  document.querySelector('#dog-list').innerHTML = '';
+  document.querySelector("#dog-list").innerHTML = "";
 
   perricosArray.forEach((dogImage) => {
     addPerrico(dogImage, false);
@@ -54,14 +53,14 @@ function renderPerricoArray() {
 const addPerrico = async (image, addToStart) => {
   const perricoImg = await getRandomDogImage();
 
-  const dogList = document.querySelector('#dog-list');
+  const dogList = document.querySelector("#dog-list");
 
-  const isAnyFilterSelected = document.querySelector('.filter-selected');
+  const isAnyFilterSelected = document.querySelector(".filter-selected");
   // Si hay algún filtro seleccionado los perritos se añaden con display none.
 
-  const dogNode = document.createElement('div');
-  dogNode.className = 'card';
-  dogNode.style.display = isAnyFilterSelected ? 'none' : '';
+  const dogNode = document.createElement("div");
+  dogNode.className = "card";
+  dogNode.style.display = isAnyFilterSelected ? "none" : "";
 
   dogNode.innerHTML = `
   <img src="${image || perricoImg}" alt="Perro" />
@@ -72,7 +71,7 @@ const addPerrico = async (image, addToStart) => {
   if (addToStart) {
     dogList.prepend(dogNode);
   } else {
-    dogList.append(dogNode);
+    dogList.append(dogNode); // También existe appendChild();
   }
   giveDogNodeListeners(dogNode);
 };
@@ -86,17 +85,17 @@ LISTENERS DE LOS BOTONES QUE AÑADEN PERRICOS.
 
 */
 
-document.querySelector('#add-1-perrico').addEventListener('click', () => {
+document.querySelector("#add-1-perrico").addEventListener("click", () => {
   addPerrico();
   clearWarningText();
 });
 
-document.querySelector('#add-perrico-start').addEventListener('click', function () {
+document.querySelector("#add-perrico-start").addEventListener("click", function () {
   addPerrico(false, true);
   clearWarningText();
 });
 
-document.querySelector('#add-5-perrico').addEventListener('click', function () {
+document.querySelector("#add-5-perrico").addEventListener("click", function () {
   clearWarningText();
 
   addPerrico();
@@ -112,47 +111,47 @@ LISTENERS DE LOS BOTONES QUE FILTRAN Y FUNCIÓN DE FILTRADO.
 
 */
 
-const likeFilterButton = document.querySelector('#like-filter');
+const likeFilterButton = document.querySelector("#like-filter");
 
-likeFilterButton.addEventListener('click', function () {
-  likeFilterButton.classList.toggle('filter-selected');
+likeFilterButton.addEventListener("click", function () {
+  likeFilterButton.classList.toggle("filter-selected");
   filterPerricos();
   //Mucho cuidado aquí con el orden de las cosas, de ir el toggle antes que el condicional, la primera ejecución se rompe siempre, ya que los oculta, adquiere la clase que permite mostrarlos y los vuelve a mostrar.
 });
 
-const dislikeFilter = document.querySelector('#dislike-filter');
+const dislikeFilter = document.querySelector("#dislike-filter");
 
-dislikeFilter.addEventListener('click', function () {
-  dislikeFilter.classList.toggle('filter-selected');
+dislikeFilter.addEventListener("click", function () {
+  dislikeFilter.classList.toggle("filter-selected");
   filterPerricos();
 });
 
 function filterPerricos() {
-  const isLikeFilterSelected = likeFilterButton.classList.contains('filter-selected');
-  const isDislikeSelected = dislikeFilter.classList.contains('filter-selected');
+  const isLikeFilterSelected = likeFilterButton.classList.contains("filter-selected");
+  const isDislikeSelected = dislikeFilter.classList.contains("filter-selected");
 
-  document.querySelectorAll('.card').forEach((dogCard) => {
+  document.querySelectorAll(".card").forEach((dogCard) => {
     // si no hay ningún filtro aplicado, lo muestra
     if (!isLikeFilterSelected && !isDislikeSelected) {
-      dogCard.style.display = '';
+      dogCard.style.display = "";
       return;
     }
 
     // si preciosismo aplicado y hay preciosisimo lo muestra
-    const likeCount = dogCard.querySelector('.like-count').innerText;
-    if (likeCount !== '' && isLikeFilterSelected) {
-      dogCard.style.display = '';
+    const likeCount = dogCard.querySelector(".like-count").innerText;
+    if (likeCount !== "" && isLikeFilterSelected) {
+      dogCard.style.display = "";
       return;
     }
 
     // si feísimo aplicado y hay feísimo lo muestra
-    const dislikeCount = dogCard.querySelector('.dislike-count').innerText;
-    if (dislikeCount !== '' && isDislikeSelected) {
-      dogCard.style.display = '';
+    const dislikeCount = dogCard.querySelector(".dislike-count").innerText;
+    if (dislikeCount !== "" && isDislikeSelected) {
+      dogCard.style.display = "";
       return;
     }
 
-    dogCard.style.display = 'none';
+    dogCard.style.display = "none";
   });
 }
 
--- Javascript en el navegador/tareas-random/index.js
+++ Javascript en el navegador/tareas-random/index.js
@@ -8,6 +8,7 @@ function generateRandomTask() {
   return {
     text: `Texto aleatorio número ${getRandomInt(1, 1000)}`,
     isCompleted: getRandomInt(0, 1) === 1,
+    isFav: getRandomInt(0, 1) === 1,
   };
 }
 
@@ -21,29 +22,61 @@ function getRandomArray() {
 
 function regenerateArray() {
   const tasks = getRandomArray();
-  document.querySelector('#tasks').innerHTML = '';
+  document.querySelector("#tasks").innerHTML = "";
 
   tasks.forEach((task) => {
     createTaskNode(task, true);
   });
 }
 
+function giveTaskListeners(task) {
+  task.addEventListener("click", (event) => {
+    if (task.querySelector(".completed")) {
+      task.querySelector(".completed").classList.toggle("completed");
+      task.querySelector(".status").innerHTML = "pending";
+      return;
+    }
+    task.querySelector("span").classList.toggle("completed");
+    task.querySelector(".status").innerHTML = "completed";
+  });
+}
+
 function createTaskNode(task, addToEnd) {
-  const taskNode = document.createElement('div');
+  const taskNode = document.createElement("div");
   //.createElement() sirve para crear elementos dinámicamente antes de agregarlos al DOM con algún append.
-  taskNode.className = 'task';
+  taskNode.className = "task";
 
   taskNode.innerHTML = `
-    <span class="${task.isCompleted ? 'completed' : ''}">${task.text}</span> -
-    <span class="status">${task.isCompleted ? 'completed' : 'pending'}</span>`;
+    <span class="${task.isCompleted ? "completed" : ""}">${task.text}</span> -
+    <span class="status">${task.isCompleted ? "completed" : "pending"}</span>
+    <button style="opacity: 0"> class="${task.isFav ? "fav" : ""}">${task.isFav ? "💝" : "💔"}</button>`;
 
-  const tasksNode = document.querySelector('#tasks');
+  const tasksNode = document.querySelector("#tasks");
 
   if (addToEnd) {
     tasksNode.appendChild(taskNode);
   } else {
     tasksNode.prepend(taskNode);
   }
+
+  taskNode.addEventListener("click", () => {
+    const taskTextNode = taskNode.querySelector("span");
+    const isCurrentlyCompleted = taskTextNode.classList.contains("completed");
+    taskTextNode.classList.toggle("completed");
+    taskNode.querySelector(".status").innerText = isCurrentlyCompleted ? "pending" : "completed";
+  });
+
+  // Tendras que usar mouseOver y mouseOut para hacer la logica del boton que aparece y desaparece.
+  // Voy a crear una clase normal y una clase hover le dare la normal y le pondre y quitare hover con un toggle.
+  // Le quitare lo de style opacity 0.
+
+  const favButton = taskNode.querySelector("button");
+  favButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    const isCurrentlyFav = favButton.classList.contains("fav");
+    favButton.classList.toggle("fav");
+    favButton.innerText = isCurrentlyFav ? "💔" : "💝";
+  });
 } //Esta función toma de parámetro si debe añadir o no la tarea al final del array, y la tarea en cuestion a añadir. Si te fijas al acceder a su innerHTML los valores de la tarea son las propiedades del parámetro correspondiente a la tarea.
 
 function addTask(addToEnd) {
@@ -52,14 +85,16 @@ function addTask(addToEnd) {
 }
 
 // event listeners para que los botones llamen a las funciones anteriores
-document.querySelector('#regenate').addEventListener('click', () => {
+document.querySelector("#regenate").addEventListener("click", () => {
   regenerateArray();
 });
 
-document.querySelector('#add-first').addEventListener('click', () => {
+document.querySelector("#add-first").addEventListener("click", () => {
   addTask();
 });
 
-document.querySelector('#add-last').addEventListener('click', () => {
+document.querySelector("#add-last").addEventListener("click", () => {
   addTask(true);
 });
+
+// Botoncico en las tareas, que nos encanta.
