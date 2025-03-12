af37857ac799caaed4c01b18694bd4bb29a77c72
Pedro Garcia <garciasan@esat-alumni.com>
2025-03-12 12:58:08 +0100

    update-2-repaso-general-12-03

--- Javascript en el navegador/Perritos-Josmi/index.js
+++ Javascript en el navegador/Perritos-Josmi/index.js
@@ -1,6 +1,6 @@
 const perricosArray = [
-  "https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg",
-  "https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg",
+  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
+  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
 ];
 //'perricosArray' es el contenedor de las imágenes de los dos primeros perricos que se pintan en pantalla por defecto al cargar la página.
 
@@ -11,14 +11,14 @@ SECCIÓN INICIAL DE TIMEOUT
 */
 
 const timeOutId = setTimeout(() => {
-  document.querySelector("#alert").style.display = "inline-block";
+  document.querySelector('#alert').style.display = 'inline-block';
 }, 3000); // A los 3 segundos muestra un texto que te dije que clickes un fucking boton.
 
 // De dejar en mi caso display vacío para que intentara mostrarse no iría. Esto solo funciona cuando ese display none lo has metido en su etiqueta de html como propiedad. Si lo metiste con CSS tienes que darle un bloque real.
 
 function clearWarningText() {
   clearTimeout(timeOutId);
-  document.querySelector("#alert").style.display = "none";
+  document.querySelector('#alert').style.display = 'none';
 } //Cuando clicas un boton llamas a esta funcion, sirve para que no se muestre el texto que dice que clickes y se carga el timeout formalmente.
 
 /*
@@ -34,7 +34,7 @@ async function populateSelect() {
   const selectNode = document.querySelector('[name = "select"]');
 
   dogBreedArray.forEach((breed) => {
-    const breedNode = document.createElement("option");
+    const breedNode = document.createElement('option');
     breedNode.value = `${breed}`;
     breedNode.innerText = `${breed}`;
 
@@ -46,16 +46,17 @@ async function populateSelect() {
     });
   });
 
-  breedToAdd = document.querySelector("option").value;
+  breedToAdd = document.querySelector('option').value;
+  //Creo que esta línea de código aquí dentro no hace nada, porque el valor base del select es vacío.
 }
 
 const breedCategoryInfo = [];
-let breedToAdd = "briard";
+let breedToAdd = 'briard';
 
 populateSelect();
 
 const selectNode = document.querySelector('[name = "select"]');
-selectNode.addEventListener("change", (event) => {
+selectNode.addEventListener('change', (event) => {
   breedToAdd = event.target.value;
 });
 
@@ -74,64 +75,64 @@ FUNCIONES BÁSICAS PARA AÑADIR PERRICOS Y DAR LISTENERS A LOS BOTONES DE SUS TA
 */
 
 const giveDogNodeListeners = (dogNode) => {
-  const likeButton = dogNode.querySelector(".like");
-  const dislikeButton = dogNode.querySelector(".dislike");
+  const likeButton = dogNode.querySelector('.like');
+  const dislikeButton = dogNode.querySelector('.dislike');
 
-  likeButton.addEventListener("click", () => {
-    const likeCountNode = dogNode.querySelector(".like-count"); //Guardamos la posición del contador positivo en una variable.
+  likeButton.addEventListener('click', () => {
+    const likeCountNode = dogNode.querySelector('.like-count'); //Guardamos la posición del contador positivo en una variable.
     likeCountNode.innerText = Number(likeCountNode.innerText) + 1; //Convertimos a número su interior y sumamos 1.
   });
 
-  dislikeButton.addEventListener("click", () => {
-    const dislikeCountNode = dogNode.querySelector(".dislike-count");
+  dislikeButton.addEventListener('click', () => {
+    const dislikeCountNode = dogNode.querySelector('.dislike-count');
     dislikeCountNode.innerText = Number(dislikeCountNode.innerText) + 1;
   });
 };
 
 function renderPerricoArray() {
-  document.querySelector("#dog-list").innerHTML = "";
+  document.querySelector('#dog-list').innerHTML = '';
 
   perricosArray.forEach((dogImage) => {
     addPerrico(dogImage, false);
   });
-}
+} //Es para pintar el array base aquel.
 
 function disableAllAddPerricoButtons() {
-  document.querySelectorAll("button").forEach((buttonNode) => {
+  document.querySelectorAll('button').forEach((buttonNode) => {
     buttonNode.disabled = true;
   });
 }
 
 function enableAllAddPerricoButtons() {
-  document.querySelectorAll("button").forEach((buttonNode) => {
+  document.querySelectorAll('button').forEach((buttonNode) => {
     buttonNode.disabled = false;
   });
 }
 
 function createRaceFilter(breed, breedcount) {
-  const breedButton = document.createElement("button");
+  const breedButton = document.createElement('button');
   breedButton.id = breed;
   breedButton.innerHTML = `${breed}(${breedcount})`;
 
-  document.querySelector(".filters").appendChild(breedButton);
+  document.querySelector('.filters').appendChild(breedButton);
 
-  breedButton.addEventListener("click", () => {
-    breedButton.classList.toggle("filter-selected");
-    document.querySelectorAll(".card").forEach((dogNode) => {
+  breedButton.addEventListener('click', () => {
+    breedButton.classList.toggle('filter-selected');
+    document.querySelectorAll('.card').forEach((dogNode) => {
       const isDogOfThisBreed = dogNode.querySelector(`[src*="${breed}"]`);
-      const isFilterSelected = breedButton.classList.contains("filter-selected");
+      const isFilterSelected = breedButton.classList.contains('filter-selected');
 
       if (!isFilterSelected) {
-        dogNode.style.display = "";
+        dogNode.style.display = '';
         return;
       }
 
       if (isDogOfThisBreed && isFilterSelected) {
-        dogNode.style.display = "";
+        dogNode.style.display = '';
         return;
       }
 
-      dogNode.style.display = "none";
+      dogNode.style.display = 'none';
     }); // El problema que enfrentamos es que no funcionan dos filtros al mismo tiempo para perritos de diferentes razas, se excluyen.
   });
 }
@@ -155,17 +156,17 @@ const addPerrico = async (image, addToStart) => {
     breedFilterNode.innerHTML = `${breedToAdd}(${breedCount})`;
   }
 
-  const dogList = document.querySelector("#dog-list");
+  const dogList = document.querySelector('#dog-list');
 
-  const isAnyFilterSelected = document.querySelector(".filter-selected");
+  const isAnyFilterSelected = document.querySelector('.filter-selected');
   // Si hay algún filtro seleccionado los perritos se añaden con display none.
 
-  const dogNode = document.createElement("div");
-  dogNode.className = "card";
-  dogNode.style.display = isAnyFilterSelected ? "none" : "";
+  const dogNode = document.createElement('div');
+  dogNode.className = 'card';
+  dogNode.style.display = isAnyFilterSelected ? 'none' : '';
 
   dogNode.innerHTML = `
-  <img src="${image || perricoImg}" alt="Perro" />
+  <img src="${image || perricoImg}" alt="Perro de raza${breedToAdd}" />
   <br />
   <p><span class="like-count"></span>❤️ <span class="dislike-count"></span>🤮</p>
   <button class="like">Preciosísimo</button> <button class="dislike">Feísisimo</button>`;
@@ -187,7 +188,7 @@ LISTENERS DE LOS BOTONES QUE AÑADEN PERRICOS.
 
 */
 
-document.querySelector("#add-1-perrico").addEventListener("click", async () => {
+document.querySelector('#add-1-perrico').addEventListener('click', async () => {
   clearWarningText();
 
   disableAllAddPerricoButtons();
@@ -195,7 +196,7 @@ document.querySelector("#add-1-perrico").addEventListener("click", async () => {
   enableAllAddPerricoButtons();
 });
 
-document.querySelector("#add-perrico-start").addEventListener("click", async function () {
+document.querySelector('#add-perrico-start').addEventListener('click', async function () {
   clearWarningText();
 
   disableAllAddPerricoButtons();
@@ -203,7 +204,7 @@ document.querySelector("#add-perrico-start").addEventListener("click", async fun
   enableAllAddPerricoButtons();
 });
 
-document.querySelector("#add-5-perrico").addEventListener("click", async function () {
+document.querySelector('#add-5-perrico').addEventListener('click', async function () {
   clearWarningText();
 
   disableAllAddPerricoButtons();
@@ -223,46 +224,46 @@ LISTENERS DE LOS BOTONES QUE FILTRAN Y FUNCIÓN DE FILTRADO.
 
 */
 
-const likeFilterButton = document.querySelector("#like-filter");
+const likeFilterButton = document.querySelector('#like-filter');
 
-likeFilterButton.addEventListener("click", function () {
-  likeFilterButton.classList.toggle("filter-selected");
+likeFilterButton.addEventListener('click', function () {
+  likeFilterButton.classList.toggle('filter-selected');
   filterPerricos();
   //Mucho cuidado aquí con el orden de las cosas, de ir el toggle antes que el condicional, la primera ejecución se rompe siempre, ya que los oculta, adquiere la clase que permite mostrarlos y los vuelve a mostrar.
 });
 
-const dislikeFilter = document.querySelector("#dislike-filter");
-dislikeFilter.addEventListener("click", function () {
-  dislikeFilter.classList.toggle("filter-selected");
+const dislikeFilter = document.querySelector('#dislike-filter');
+dislikeFilter.addEventListener('click', function () {
+  dislikeFilter.classList.toggle('filter-selected');
   filterPerricos();
 });
 
 function filterPerricos() {
-  const isLikeFilterSelected = likeFilterButton.classList.contains("filter-selected");
-  const isDislikeSelected = dislikeFilter.classList.contains("filter-selected");
+  const isLikeFilterSelected = likeFilterButton.classList.contains('filter-selected');
+  const isDislikeSelected = dislikeFilter.classList.contains('filter-selected');
 
-  document.querySelectorAll(".card").forEach((dogCard) => {
+  document.querySelectorAll('.card').forEach((dogCard) => {
     // si no hay ningún filtro aplicado, lo muestra
     if (!isLikeFilterSelected && !isDislikeSelected) {
-      dogCard.style.display = "";
+      dogCard.style.display = '';
       return;
     }
 
     // si preciosismo aplicado y hay preciosisimo lo muestra
-    const likeCount = dogCard.querySelector(".like-count").innerText;
-    if (likeCount !== "" && isLikeFilterSelected) {
-      dogCard.style.display = "";
+    const likeCount = dogCard.querySelector('.like-count').innerText;
+    if (likeCount !== '' && isLikeFilterSelected) {
+      dogCard.style.display = '';
       return;
     }
 
     // si feísimo aplicado y hay feísimo lo muestra
-    const dislikeCount = dogCard.querySelector(".dislike-count").innerText;
-    if (dislikeCount !== "" && isDislikeSelected) {
-      dogCard.style.display = "";
+    const dislikeCount = dogCard.querySelector('.dislike-count').innerText;
+    if (dislikeCount !== '' && isDislikeSelected) {
+      dogCard.style.display = '';
       return;
     }
 
-    dogCard.style.display = "none";
+    dogCard.style.display = 'none';
   });
 }
 
@@ -281,7 +282,7 @@ const intervalId = setInterval(() => {
   if (automaticPerrosCount === 2) {
     clearInterval(intervalId);
   }
-}, 5000); //Añade un perrico cada 10 segundos, tras añadir dos para.
+}, 5000); //Añade un perrico cada 5 segundos, tras añadir dos para.
 
 renderPerricoArray(); //Renderiza los dos perricos iniciales.
 
