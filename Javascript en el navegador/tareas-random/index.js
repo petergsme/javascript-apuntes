129a7c84cecaba917c6febfb5ec2d574f1094242
Pedro Garcia <itsbypeteryt@gmail.com>
2025-03-12 12:24:29 +0100

    updated-repaso-general-12-03

--- Javascript en el navegador/tareas-random/index.js
+++ Javascript en el navegador/tareas-random/index.js
@@ -22,7 +22,7 @@ function getRandomArray() {
 
 function regenerateArray() {
   const taskList = getRandomArray();
-  document.querySelector("#tasks").innerHTML = "";
+  document.querySelector('#tasks').innerHTML = '';
   localStorage.clear(); // Regenerar el array limpia el localStorage.
 
   taskList.forEach((task) => {
@@ -31,16 +31,16 @@ function regenerateArray() {
 }
 
 function createTaskNode(task, addToEnd) {
-  const taskNode = document.createElement("div");
+  const taskNode = document.createElement('div');
   //.createElement() sirve para crear elementos dinámicamente antes de agregarlos al DOM con algún append.
-  taskNode.className = "task";
+  taskNode.className = 'task';
 
   taskNode.innerHTML = `
-    <span class="${task.isCompleted ? "completed" : ""}">${task.text}</span> -
-    <span class="status">${task.isCompleted ? "completed" : "pending"}</span>
-    <button class="${task.isFav ? "fav" : ""}" style="opacity:0">${task.isFav ? "💝" : "💔"}</button>`;
+    <span class="${task.isCompleted ? 'completed' : ''}">${task.text}</span> -
+    <span class="status">${task.isCompleted ? 'completed' : 'pending'}</span>
+    <button class="${task.isFav ? 'fav' : ''}" style="opacity:0">${task.isFav ? '💝' : '💔'}</button>`;
 
-  const taskListNode = document.querySelector("#tasks");
+  const taskListNode = document.querySelector('#tasks');
 
   if (addToEnd) {
     taskListNode.appendChild(taskNode);
@@ -48,23 +48,23 @@ function createTaskNode(task, addToEnd) {
     taskListNode.prepend(taskNode);
   }
 
-  taskNode.addEventListener("click", () => {
-    const taskTextNode = taskNode.querySelector("span");
-    const isCurrentlyCompleted = taskTextNode.classList.contains("completed");
-    taskTextNode.classList.toggle("completed");
-    taskNode.querySelector(".status").innerText = isCurrentlyCompleted ? "pending" : "completed";
+  taskNode.addEventListener('click', () => {
+    const taskTextNode = taskNode.querySelector('span');
+    const isCurrentlyCompleted = taskTextNode.classList.contains('completed');
+    taskTextNode.classList.toggle('completed');
+    taskNode.querySelector('.status').innerText = isCurrentlyCompleted ? 'pending' : 'completed';
 
     const taskInLocal = JSON.parse(localStorage.getItem(`tarea${task.id}`));
     taskInLocal.isCompleted = !taskInLocal.isCompleted;
     localStorage.setItem(`tarea${taskInLocal.id}`, JSON.stringify(taskInLocal));
   });
 
-  const favButton = taskNode.querySelector("button");
-  favButton.addEventListener("click", (event) => {
+  const favButton = taskNode.querySelector('button');
+  favButton.addEventListener('click', (event) => {
     event.stopPropagation();
-    const isCurrentlyFav = favButton.classList.contains("fav");
-    favButton.classList.toggle("fav");
-    favButton.innerText = isCurrentlyFav ? "💔" : "💝";
+    const isCurrentlyFav = favButton.classList.contains('fav');
+    favButton.classList.toggle('fav');
+    favButton.innerText = isCurrentlyFav ? '💔' : '💝';
 
     const taskInLocal = JSON.parse(localStorage.getItem(`tarea${task.id}`));
     taskInLocal.isFav = !taskInLocal.isFav;
@@ -73,13 +73,13 @@ function createTaskNode(task, addToEnd) {
 
   // ¿Por qué setItem no mueve la clave en localStorage cuando actualizo una tarea? técnicamente no es la misma tarea.
 
-  // Porque setItem no borra y vuelve a crear la clave, solo actualiza su valor. localStorage no mantiene un orden basado en el tiempo de modificación, sino en cómo el navegador lo maneja internamente. Para mantener el orden, es mejor usar un array dentro de una sola clave.
+  // Porque setItem no borra y vuelve a crear la clave, solo actualiza su valor. localStorage no mantiene un orden basado en el tiempo de modificación, sino en cómo el navegador lo maneja internamente. Para mantener el orden, es mejor usar un gran array dentro de una única clave.
 
-  favButton.addEventListener("mouseover", () => {
+  favButton.addEventListener('mouseover', () => {
     favButton.style.opacity = 1;
   });
 
-  favButton.addEventListener("mouseout", () => {
+  favButton.addEventListener('mouseout', () => {
     favButton.style.opacity = 0;
   });
 } //Esta función toma de parámetro si debe añadir o no la tarea al final del array, y la tarea en cuestion a añadir. Si te fijas al acceder a su innerHTML los valores de la tarea son las propiedades del parámetro correspondiente a la tarea.
@@ -90,25 +90,25 @@ function addTask(addToEnd) {
 } // Esta función es algo redundante, los eventlistener podrian utilizar createTaskNode(generateRandomTask(), true); La única ventaja de hacerlo con addTask es que es más semántico, pero es un poco lioso.
 
 // Event listeners para que los botones llamen a las funciones anteriores
-document.querySelector("#regenerate").addEventListener("click", () => {
+document.querySelector('#regenerate').addEventListener('click', () => {
   regenerateArray();
 });
 
 let counter = 0;
-const counterInLocal = localStorage.getItem("counter");
+const counterInLocal = localStorage.getItem('counter');
 
-if (counterInLocal > 1) {
+if (counterInLocal >= 1) {
   counter = counterInLocal;
 }
 
-const formNode = document.querySelector("#create-task");
+const formNode = document.querySelector('#create-task');
 
-formNode.addEventListener("submit", (event) => {
+formNode.addEventListener('submit', (event) => {
   event.preventDefault(); // Enviar un formulario recarga la página, esto lo evita.
 
   const formData = new FormData(event.target);
   // FormData es un objeto de JavaScript que permite recolectar los valores de un formulario de forma sencilla sin acceder a cada input individualmente.
-  const taskText = formData.get("taskText");
+  const taskText = formData.get('taskText');
   // .get("nombreDelCampo") obtiene el valor de un campo del formulario.
 
   counter++;
@@ -122,16 +122,16 @@ formNode.addEventListener("submit", (event) => {
   createTaskNode(newTask);
   formNode.reset();
 
-  localStorage.setItem("counter", counter);
+  localStorage.setItem('counter', counter);
 
   localStorage.setItem(`tarea${newTask.id}`, JSON.stringify(newTask));
 });
 
-const inputNode = document.querySelector("#create-task > input");
-inputNode.addEventListener("input", (event) => {
+const inputNode = document.querySelector('#create-task > input');
+inputNode.addEventListener('input', (event) => {
   const inputText = event.target.value;
 
-  document.querySelector("#create-task button").disabled = !inputText.length;
+  document.querySelector('#create-task button').disabled = !inputText.length;
 });
 
 function reloadSession() {
@@ -150,45 +150,45 @@ function reloadSession() {
 
 reloadSession();
 
-const completedFilterButton = document.querySelector("#show-completed");
+const completedFilterButton = document.querySelector('#show-completed');
 
-completedFilterButton.addEventListener("click", function () {
-  completedFilterButton.classList.toggle("filter-selected");
+completedFilterButton.addEventListener('click', function () {
+  completedFilterButton.classList.toggle('filter-selected');
   filterTasks();
 });
 
-const pendingFilterButton = document.querySelector("#show-pending");
+const pendingFilterButton = document.querySelector('#show-pending');
 
-pendingFilterButton.addEventListener("click", function () {
-  pendingFilterButton.classList.toggle("filter-selected");
+pendingFilterButton.addEventListener('click', function () {
+  pendingFilterButton.classList.toggle('filter-selected');
   filterTasks();
 });
 
 function filterTasks() {
-  const isCompletedFilterSelected = completedFilterButton.classList.contains("filter-selected");
-  const isPendingFilterSelected = pendingFilterButton.classList.contains("filter-selected");
+  const isCompletedFilterSelected = completedFilterButton.classList.contains('filter-selected');
+  const isPendingFilterSelected = pendingFilterButton.classList.contains('filter-selected');
 
-  document.querySelectorAll(".task").forEach((taskNode) => {
+  document.querySelectorAll('.task').forEach((taskNode) => {
     // si no hay ningún filtro aplicado, lo muestra
     if (!isCompletedFilterSelected && !isPendingFilterSelected) {
-      taskNode.style.display = "";
+      taskNode.style.display = '';
       return;
     }
 
-    // si preciosismo aplicado y hay preciosisimo lo muestra
-    const isCompleted = taskNode.querySelector("span").classList.contains("completed");
+    // si completada aplicado y hay completada lo muestra
+    const isCompleted = taskNode.querySelector('span').classList.contains('completed');
     if (isCompleted && isCompletedFilterSelected) {
-      taskNode.style.display = "";
+      taskNode.style.display = '';
       return;
     }
 
-    // si feísimo aplicado y hay feísimo lo muestra
+    // si pendiente aplicado y hay pendiente lo muestra
     if (!isCompleted && isPendingFilterSelected) {
-      taskNode.style.display = "";
+      taskNode.style.display = '';
       return;
     }
 
-    taskNode.style.display = "none";
+    taskNode.style.display = 'none';
   });
 }
 
@@ -224,13 +224,3 @@ Esto funciona porque nosotros, al recargar la página recuperamos las tareas de 
 */
 
 // Cuando trabajas con strings/booleans/enteros en memoria se guarda el valor. Pero con objetos se guarda la posicion en memoria, la referencia del objeto. Cambiar el valor cambia referencia en memoria, pero con objetos no.
-
-// Boton de filtro para mostrar las tareas completadas.
-
-/*
-
-
-
-
-
-*/
