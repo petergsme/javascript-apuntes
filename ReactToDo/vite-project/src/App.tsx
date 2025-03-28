70feaad15e52f32dd02187bfb96c8dafef400c7a
Pedro Garcia <garciasan@esat-alumni.com>
2025-03-28 14:09:46 +0100

    update-React4-home-28-03

--- ReactToDo/vite-project/src/App.tsx
+++ ReactToDo/vite-project/src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 interface Task {
   taskName: string;
@@ -7,33 +7,53 @@ interface Task {
 }
 
 function App() {
-  const [tasksArray, setTasksArray] = useState<Task[]>([
-    {
-      taskName: "Cepillar los dientes",
-      isCompleted: true,
-    },
-  ]);
+  const [tasksArray, setTasksArray] = useState<Task[]>([]);
+  // Esta es la manera de especificar un tipado en un useState.
+  const [inputText, setInputText] = useState('Escribe una tarea');
 
-  const singleTask: Task = ({ taskName, isCompleted = false }) => {
-    return (
-      <li>
-        <input type="checkbox" checked={isCompleted} />
-        {taskName}
-        <button>eliminar tarea</button>
-      </li>
-    );
+  const handleClick = (text: string) =>
+    setTasksArray([
+      ...tasksArray,
+      {
+        taskName: text,
+        isCompleted: true,
+      },
+    ]);
+
+  const handleDelete = (task: Task) => {
+    setTasksArray(tasksArray.filter((tarea) => tarea.taskName !== task.taskName));
   };
 
-  console.log(tasksArray);
-  console.log(setTasksArray([...tasksArray]));
+  const handleCompleted = (task: Task) => {
+    setTasksArray(
+      tasksArray.map((tarea) =>
+        tarea.taskName === task.taskName ? { ...tarea, isCompleted: !tarea.isCompleted } : tarea
+      )
+    );
+  };
 
   return (
     <>
       <div>
-        <input type="text" />
-        <button>Añadir tarea</button>
+        <input
+          type="text"
+          value={inputText}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setInputText(event.target.value)}
+        />
+        {/* Hacer hover sobre el event te dice el event que jsx cree que le toca, sin tipado se rompe*/}
+        <button onClick={() => handleClick(inputText)}>Añadir tarea</button>
       </div>
-      <ul className="task-list">{singleTask("Julian", false)}</ul>
+      <ul className="task-list">
+        {tasksArray.map((task) => {
+          return (
+            <li key={task.taskName}>
+              <input type="checkbox" checked={task.isCompleted} onClick={() => handleCompleted(task)} />
+              {task.taskName}
+              <button onClick={() => handleDelete(task)}>eliminar tarea</button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
